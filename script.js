8947c44cc85bf0fc07ff0876e1a1af62788c4e99
Ckey230 <Ckey230@gmail.com>
2022-10-30 19:55:36 -0400

    fixed positioning issue game prompt mockup

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function removeJump(){
     character.classList.remove("animate");
 }
 
+
+
+var block = document.getElementById("block");
+function checkDead(){
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<10 && blockLeft>-5 && characterTop>=230){
+        alert("Game over");
+    }
+}
+
+setInterval(checkDead, 10);

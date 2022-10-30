24bbe2fbfe13b6a68a699150377ec298fc6136ef
Ckey230 <Ckey230@gmail.com>
2022-10-30 13:30:22 -0400

    character jump

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var character = document.getElementById("character");
 document.addEventListener("click",jump);
 function jump(){
+    
     character.classList.add("animate");
     setTimeout(removeJump,300); //300ms = length of animation
 };

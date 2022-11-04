27d05d1e7e5f34942020e40bb9669eeac2ef799c
Ckey230 <Ckey230@gmail.com>
2022-11-03 21:01:19 -0400

    obstacles finished

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+
 document.addEventListener('DOMContentLoaded', () => {
+    var canvas = document.querySelector('canvas');
+  
     const character = document.querySelector(".character");
-    const game = document.querySelector('.block')
+    const game = document.querySelector('.game')
+    const prompt = document.getElementById('prompt')
     let characterJump = false
     let gravity = 0.9
+    let isGameOver = false
 
 
+//space bar for jumping
     function control(e){
         if (e.keyCode === 32){
             if (!characterJump){
@@ -17,6 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('keydown', control)
     let position = 0
 
+//jump time function
     function jump(){
         let count = 0 
         let timerId = setInterval(function () {
@@ -40,17 +47,42 @@ document.addEventListener('DOMContentLoaded', () => {
             
 
                 count ++
-                position += 30
+                position += 40
                 position = position * gravity
               character.style.bottom = position + 'px'
             
             
 
-        }, 30)
+        }, 15)
     }
+   //blocks generated
+      function generateBlock() {
+        let randomTime = Math.random() * 2500
+        let blockPosition = 1000
+        const block = document.createElement('div')
+       if (!isGameOver) block.classList.add('block')
+        game.appendChild(block)
+        block.style.left = blockPosition + 'px'
+        // timer for blocks
+        let timerId = setInterval(function (){
+            if (blockPosition > 0 && blockPosition < 60 && position < 60) {
+                clearInterval(timerId)
+                prompt.innerHTML = 'Game Over'
+                isGameOver = true
+                //remove div's after 'game over'
+                while (game.firstChild) {
+                    game.removeChild(game.lastChild)
+                }
+            }
+            blockPosition -= 7
+            block.style.left = blockPosition + 'px'
+         
+        },15)
+       if (!isGameOver) setTimeout(generateBlock, randomTime)
+      }
+      generateBlock()
    
-
-
+   
 
 
 
